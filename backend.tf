67dcb9ee7c55e91950972e1586602d0ba4dbecb2
cndeke <chibuzorndeke@yahoo.com>
2023-06-18 05:40:55 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,6 +4,7 @@ terraform {
   backend "s3" {
     bucket = "week-24-project-12022021"
     key    = "remote.tfstate"
+    profile = "default"
     region = "us-east-1"
     dynamodb_table = "jjtfstate"
   }
